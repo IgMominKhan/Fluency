5deabdd506f88eab36563ae22f93cdfa523a60c7
IgMominKhan <mominkhan8991@gmail.com>
2023-06-17 18:39:08 +0300

    popular instructor section url changed

--- src/Pages/Home/PopularInstructors.jsx
+++ src/Pages/Home/PopularInstructors.jsx
@@ -5,24 +5,23 @@ import axios from "axios";
 const PopularInstructors = () => {
   const [popularInstructors, setPopularInstructors] = useState([]);
   useEffect(() => {
-    axios.get("./teachers.json").then((res) =>
-      setPopularInstructors(res.data)
-    );
+    axios.get("https://fluency-server.vercel.app/users?role=instructor").then((
+      res,
+    ) => setPopularInstructors(res.data.slice(0, 6)));
   }, []);
   return (
     <section className="bg-clr-accent dark:bg-black/90 dark:text-white">
-    <div className="main-container">
-      <h1 className="title text-center ">
-        Our <span className="text-clr-primary">Popular</span> Instructors
-      </h1>
-      <div className="my-12 grid gap-7  grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] justify-center">
-        {popularInstructors.map((instructor, i) => (
-          <InstructorCard key={i} instructor={instructor} />
-        ))}
+      <div className="main-container">
+        <h1 className="title text-center ">
+          Our <span className="text-clr-primary">Popular</span> Instructors
+        </h1>
+        <div className="my-12 grid gap-7  grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] justify-center">
+          {popularInstructors.map((instructor, i) => (
+            <InstructorCard key={i} instructor={instructor} />
+          ))}
+        </div>
       </div>
-    </div>  
     </section>
-    
   );
 };
 
